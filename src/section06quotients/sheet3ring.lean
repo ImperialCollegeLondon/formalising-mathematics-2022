3dddc35c506090030dff83e11bec6dcd78338ce0
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-02-12 16:41:51 +0000

    add sheet 3

--- src/section06quotients/sheet3ring.lean
+++ src/section06quotients/sheet3ring.lean
@@ -5,20 +5,25 @@ Author : Kevin Buzzard
 -/
 
 import tactic -- imports all the Lean tactics
-import section06quotients.sheet2zmod37
+import solutions.section06quotients.sheet2zmod37
 
 /-!
 
 # Z/37 is a ring
 
+(or an additive abelian group if you only get half way through the sheet)
+
 -/
 
 namespace Zmod
 
--- We already defined negation. Let's set up notation for it.
+-- We already defined negation but right now it's called `neg`. 
+-- Let's set up notation for it so we can call it `-`.
 
 instance : has_neg Zmod37 :=
 { neg := neg }
+
+-- Now we can talk about `-⟦a⟧` and Lean will interpret it as `neg ⟦a⟧`.
 
 -- Let's train the simplifier to push everything into
 -- the brackets
@@ -36,19 +41,38 @@ begin
   refl
 end
 
--- Finally addition. This is a map `Zmod37 → Zmod37 → Zmod37` so we 
--- can use a function in the library called `quotient.map₂` to descend
--- addition on the integers; we will need to prove a result saying
--- it's well-defined though.
+/-
+
+## Maps X → X → X
+
+We're trying to make `Zmod37` into an additive group; we've defined
+the additive unit `0` and additive inverse `-`; we still need to 
+define the group law `+` though. The type of `+` will be
+`Zmod37 → Zmod37 → Zmod37` and this might look a bit weird, so let
+me just spell it out. We usually think of addition as a function
+which eats two elements of an additive group `G` and returns another
+element. But here what we're going to do is to think of addition
+as a function `g + _` which eats *one* element `g` and returns a
+function `G → G`, namely the function which adds `g` to its input.
+With this way of thinking about it, addition is a function
+from `Zmod37` to the type of functions from `Zmod37` to `Zmod37`,
+and this latter type is called `Zmod37 → Zmod37`. So, putting
+it all together, addition has type `Zmod37 → (Zmod37 → Zmod37)`,
+and because `→` is right associative in Lean we can write this
+as `Zmod37 → Zmod37 → Zmod37` (indeed, that's what "right associative" means).
+
+-/
+
+-- We define addition using a function in the library called `quotient.map₂`
+-- which will descend a term of type `ℤ → ℤ → ℤ` to a term of type
+-- `Zmod37 → Zmod37 → Zmod37`. We descend addition on the integers.
+-- To make this work (to check it's a "well-defined definition") we need to prove
+-- a theorem, hence this starts as a definition and ends up as a proof of a theorem.
 
 def add : Zmod37 → Zmod37 → Zmod37 :=
 quotient.map₂ (λ a b, a + b) begin
-  -- keep introsing, and dsimp the lambdas away
-  rintros a b ⟨c, hc⟩ d e ⟨f, hf⟩,
-  refine ⟨c + f, _⟩,
-  dsimp,
-  rw [mul_add, ←hc, ←hf],
-  ring,
+  -- keep intro-ing, and dsimp the lambdas away
+  sorry
 end
 
 instance : has_add Zmod37 :=
@@ -59,47 +83,59 @@ begin
   refl
 end
 
+/-
+
+## `quotient.induction_on`
+
+The one thing I've not told you about is a really nice way of 
+reducing questions about `Zmod37` to equations about integers.
+Sure you can use `surjective_quotient_mk` but there's a much
+nicer way; you can do "induction on the terms of the quotient".
+Let me do this first one for you, to give you the idea of
+how to do these simply.
+
+-/
 lemma add_zero (z : Zmod37) : z + 0 = z :=
 begin
+  -- A question about Zmod37, a type we're in the middle of building
+  -- an API for.
   apply quotient.induction_on z, clear z,
+  -- Still a question about Zmod37, but now all our variables
+  -- are integers,
   intro a,
+  -- We just trained the simplifier to do questions like these.
+  -- We told it `0 = ⟦0⟧` and `⟦a⟧ + ⟦b⟧ = ⟦a + b⟧`.
   simp,
 end
 
+-- I'll give you the first line for this one.
 lemma add_comm (y z : Zmod37) : y + z = z + y :=
 begin
   apply quotient.induction_on₂ y z, clear y z,
-  intros,
-  simp,
-  use 0,
-  ring,
+  sorry
 end
 
+-- See if you can prove the remaining axioms for an additive abelian group yourself.
+-- We name this instance `Zmod.add_comm_group` and we'll use it later on.
 instance add_comm_group : add_comm_group Zmod37 :=
 { add := (+),
   zero := 0,
   neg := has_neg.neg,
-  add_assoc := begin     intros a b c,
-    apply quotient.induction_on₃ a b c,
-    clear a b c,
-    intros a b c,
-    simp,
-    use 0,
-    ring, end,
-  zero_add := begin     intro a,
-    apply quotient.induction_on a,
-    clear a,
-    intro a,
-    simp, end,
+  add_assoc := begin
+    sorry
+  end,
+  zero_add := begin
+    sorry
+  end,
   add_zero := add_zero,
-  add_left_neg := begin     intro a,
-    apply quotient.induction_on a,
-    clear a,
-    intro a,
-    simp, end,
+  add_left_neg := begin
+    sorry
+  end,
   add_comm := add_comm }
 
--- Think you can make it a ring?
+-- Bonus points: see if you can make it a ring!
+-- The rest of this file introduces no new techniques; all it offers
+-- you is the satisfaction of proving that ℤ/37ℤ is a ring by yourself.
 
 instance : has_one Zmod37 :=
 { one := ⟦1⟧ }
@@ -111,17 +147,8 @@ end
 
 def mul : Zmod37 → Zmod37 → Zmod37 :=
 quotient.map₂ (λ x y, x * y) begin
-  rintros a b ⟨c, hc⟩ d e ⟨f, hf⟩,
-  dsimp only,
-  -- a = b + 37 * c
-  -- d = e + 37 * f
-  use f * b + c * d,
-  rw mul_add,
-  rw ← mul_assoc,
-  rw ← hf,
-  rw ← mul_assoc,
-  rw ← hc,
-  ring,
+  -- tricky!
+  sorry,
 end
 
 instance : has_mul Zmod37 :=
@@ -137,54 +164,25 @@ instance : comm_ring Zmod37 :=
   mul := (*),
   add := (+),
   mul_assoc := begin
-    intros a b c,
-    apply quotient.induction_on₃ a b c,
-    clear a b c,
-    intros a b c,
-    simp,
-    use 0,
-    ring,
+    sorry
   end,
   one := 1,
   one_mul := begin
-    intro a,
-    apply quotient.induction_on a,
-    clear a,
-    intro a,
-    simp,
+    sorry
   end,
   mul_one := begin
-    intro a,
-    apply quotient.induction_on a,
-    clear a,
-    intro a,
-    simp,
+    sorry
   end,
   left_distrib := begin
-    intros a b c,
-    apply quotient.induction_on₃ a b c,
-    clear a b c,
-    intros a b c,
-    simp,
-    use 0,
-    ring,
+    sorry,
   end,
   right_distrib := begin
-    intros a b c,
-    apply quotient.induction_on₃ a b c,
-    clear a b c,
-    intros a b c,
-    simp,
-    use 0,
-    ring,
+    sorry,
   end,
   mul_comm := begin
-    intros y z,
-    apply quotient.induction_on₂ y z, clear y z,
-    intros,
-    simp,
-    use 0,
-    ring,
+    sorry,
 end,
+  -- the rest of the ring axioms are the axioms for an additive abelian group,
+  -- and we did those already.
   ..Zmod.add_comm_group }
 end Zmod

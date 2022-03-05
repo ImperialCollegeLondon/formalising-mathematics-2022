ef7ac961cfe5907f8b56d73a902f9561e9314f7d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-03-05 22:40:48 +0000

    fix setoid howler (thanks Jason)

--- src/section13modules/picard_with_ideals.lean
+++ src/section13modules/picard_with_ideals.lean
@@ -13,16 +13,11 @@ variables (R : Type u) [comm_ring R]
 -- ideal version
 
 def ideal.s : setoid (ideal R) :=
-{ r := λ I J, nonempty (I ≃ₗ[R] I),
-  iseqv := begin
-    refine ⟨_, _, _⟩,
-    { intro x,
-      exact has_one.nonempty, },
-    { intros x y h,
-      exact has_one.nonempty, },
-    { intros x y z h1 h2,
-      exact has_one.nonempty, }
-  end }
+{ r := λ I J, nonempty (I ≃ₗ[R] J),
+  iseqv :=
+  ⟨λ I, nonempty.intro $ linear_equiv.refl _ _, 
+   λ I J hIJ, nonempty.intro $ hIJ.some.symm, 
+   λ I J K hIJ hJK, nonempty.intro $ hIJ.some.trans hJK.some⟩ }
 
 def ideal.Picard_monoid := quotient (ideal.s R)
  

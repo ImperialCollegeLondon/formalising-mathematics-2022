c0ecc5e165e181c99f526fd085be9141b4314d54
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-03-06 07:34:00 +0000

    more ideasl

--- src/section13modules/picard_with_ideals.lean
+++ src/section13modules/picard_with_ideals.lean
@@ -22,54 +22,221 @@ def s : setoid (ideal R) :=
    λ I J hIJ, nonempty.intro $ hIJ.some.symm, 
    λ I J K hIJ hJK, nonempty.intro $ hIJ.some.trans hJK.some⟩ }
 
-/-- The Picard monoid `ideal.Picard_monoid R` as a bare type -/
-abbreviation Picard_monoid := quotient (ideal.s R)
+/-
 
--- Right now I don't know if I can give it a monoid structure though!
+The Picard monoid is the quotient by `s` as a bare type.
 
--- We could try to use `con.quotient`. Is `s` a congruence relation?
--- with respect to multiplication of ideals?
+But let's make the quotient in a different way using `con`.
 
-def mul (I J : ideal R) : ideal R := ideal.span {r : R | ∃ (i ∈ I) (j ∈ J), r = i * r}
+-/
+def mul (I J : ideal R) : ideal R := ideal.span {r : R | ∃ (i ∈ I) (j ∈ J), r = i * j}
 
 instance : has_mul (ideal R) :=
 { mul := mul R }
 
--- Here's the problem I ran into when I tried to prove this:
-example : con (ideal R) :=
+instance : has_one (ideal R) :=
+{ one := ⊤ }
+
+instance : monoid (ideal R) :=
+{ mul := (*),
+  one := 1,
+  mul_assoc := λ I J K, begin
+    apply le_antisymm,
+    { apply span_le.2 _,
+      rintro - ⟨i, hiIJ, k, hk, rfl⟩,
+      apply submodule.span_induction hiIJ,
+      { rintro - ⟨i, hi, j, hj, rfl⟩,
+        apply subset_span,
+        refine ⟨i, hi, j * k, subset_span ⟨j, hj, k, hk, rfl⟩, mul_assoc i j k⟩ },
+      { simp },
+      { intros a b ha hb, 
+        simp [add_mul, ideal.add_mem (I * (J * K)) ha hb] },
+      { intros a x h, change _ ∈ I * (J * K), 
+        convert submodule.smul_mem (I * (J * K)) a h using 1, 
+        simp [mul_assoc] } },
+    { apply span_le.2 _,
+      rintro - ⟨i, hi, jk, hjk, rfl⟩,
+      apply submodule.span_induction hjk,
+      { rintro - ⟨j, hj, k, hk, rfl⟩,
+        apply subset_span,
+        refine ⟨i * j, subset_span ⟨i, hi, j, hj, rfl⟩, 
+          k, hk, (mul_assoc i j k).symm⟩, },
+      { simp },
+      { intros a b ha hb, 
+        simp [mul_add, ideal.add_mem (I * J * K) ha hb], },
+      { intros a x h, change _ ∈ I * J * K, 
+        convert submodule.smul_mem (I * J * K) a h using 1, 
+        simp [← mul_assoc, mul_left_comm], } },
+  end,
+  one_mul := begin 
+    intro I,
+    apply le_antisymm,
+    { apply span_le.2 _,
+      rintro - ⟨r, -, i, hi, rfl⟩,
+      exact submodule.smul_mem I r hi },
+    { intros i hi,
+      apply subset_span,
+      refine ⟨1, submodule.mem_top, i, hi, (one_mul i).symm⟩ },
+  end,
+  mul_one := λ I, begin 
+    apply le_antisymm,
+    { apply span_le.2 _,
+      rintro - ⟨i, hi, r, -, rfl⟩,
+      exact I.mul_mem_right r hi },
+    { intros i hi,
+      apply subset_span,
+      refine ⟨i, hi, 1, submodule.mem_top, (mul_one i).symm⟩, },
+    end, }
+
+lemma mul_le_left {I J : ideal R} : I * J ≤ I :=
+begin
+  apply span_le.2,
+  rintro a ⟨i, hi, j, hj, rfl⟩,
+  apply ideal.mul_mem_right _ _ hi,
+end
+
+lemma mul_le_right {I J : ideal R} : I * J ≤ J :=
+begin
+  apply span_le.2,
+  rintro a ⟨i, hi, j, hj, rfl⟩,
+  apply ideal.mul_mem_left _ _ hj,
+end
+
+variable {R}
+
+def linear_map.rmul (I : ideal R) {J K : ideal R} (e : J →ₗ[R] K) : 
+  (J * I : ideal R) →ₗ[R] (K * I : ideal R) :=
+{ to_fun := λ x, ⟨e.to_fun ⟨x.1, mul_le_left R x.2⟩, begin
+    refine submodule.span_induction' _ _ _ _ x; clear x,
+    { rintro - ⟨j, hj, i, hi, rfl⟩, 
+      apply submodule.subset_span, 
+      refine ⟨e ⟨j, hj⟩, (e ⟨j, hj⟩).2, i, hi, _⟩,
+      simp_rw mul_comm j i,
+      rw mul_comm (e ⟨j, hj⟩ : R) i,
+      have h := e.map_smul i ⟨j, hj⟩,
+      apply_fun subtype.val at h,
+      exact h },
+    { convert ideal.zero_mem _,
+      simp,
+      exact e.map_zero },
+    { rintros ⟨x, hx⟩ ⟨y, hy⟩ hex hey,
+      convert (K * I).add_mem hex hey,
+      simp only [submodule.coe_add, submodule.coe_mk, linear_map.to_fun_eq_coe, subtype.val_eq_coe],
+      have := e.map_add ⟨x, mul_le_left R hx⟩ ⟨y, mul_le_left R hy⟩,
+      apply_fun subtype.val at this,
+      exact this },
+    { rintros r ⟨x, hx⟩ hex,
+      convert (K * I).smul_mem r hex,
+      simp,
+      have := e.map_smul r ⟨x, mul_le_left R hx⟩,
+      apply_fun subtype.val at this,
+      exact this }
+  end⟩,
+  map_add' := begin
+    rintros ⟨x, hx⟩ ⟨y, hy⟩, 
+    apply subtype.coe_injective,
+    have := e.map_add ⟨x, mul_le_left R hx⟩ ⟨y, mul_le_left R hy⟩,
+    apply_fun subtype.val at this,
+    exact this,
+  end,
+  map_smul' := begin
+    rintros r ⟨x, hx⟩,
+    apply subtype.coe_injective,
+    have := e.map_smul r ⟨x, mul_le_left R hx⟩,
+    apply_fun subtype.val at this,
+    exact this,
+  end }
+
+def linear_equiv.rmul (I : ideal R) {J K : ideal R} (e : J ≃ₗ[R] K) : 
+  (J * I : ideal R) ≃ₗ[R] (K * I : ideal R) :=
+{ inv_fun := linear_map.rmul I e.symm.to_linear_map,
+  left_inv := begin
+    intro x,
+    simp [linear_map.rmul],
+  end,
+  right_inv := begin
+    rintro ⟨x, hx⟩,
+    simp [linear_map.rmul],
+  end,
+  ..linear_map.rmul I e.to_linear_map }
+
+def linear_map.lmul (I : ideal R) {J K : ideal R} (e : J →ₗ[R] K) : 
+  (I * J : ideal R) →ₗ[R] (I * K : ideal R) :=
+{ to_fun := λ x, ⟨e.to_fun ⟨x.1, mul_le_right R x.2⟩, begin
+    refine submodule.span_induction' _ _ _ _ x; clear x,
+    { rintro - ⟨i, hi, j, hj, rfl⟩, 
+      apply submodule.subset_span, 
+      refine ⟨i, hi, e ⟨j, hj⟩, (e ⟨j, hj⟩).2, _⟩,
+      have h := e.map_smul i ⟨j, hj⟩,
+      apply_fun subtype.val at h,
+      exact h },
+    { convert ideal.zero_mem _,
+      simp,
+      exact e.map_zero },
+    { rintros ⟨x, hx⟩ ⟨y, hy⟩ hex hey,
+      convert (I * K).add_mem hex hey,
+      simp only [submodule.coe_add, submodule.coe_mk, linear_map.to_fun_eq_coe, subtype.val_eq_coe],
+      have := e.map_add ⟨x, mul_le_right R hx⟩ ⟨y, mul_le_right R hy⟩,
+      apply_fun subtype.val at this,
+      exact this },
+    { rintros r ⟨x, hx⟩ hex,
+      convert (I * K).smul_mem r hex,
+      simp,
+      have := e.map_smul r ⟨x, mul_le_right R hx⟩,
+      apply_fun subtype.val at this,
+      exact this }
+  end⟩,
+  map_add' := begin
+    rintros ⟨x, hx⟩ ⟨y, hy⟩, 
+    apply subtype.coe_injective,
+    have := e.map_add ⟨x, mul_le_right R hx⟩ ⟨y, mul_le_right R hy⟩,
+    apply_fun subtype.val at this,
+    exact this,
+  end,
+  map_smul' := begin
+    rintros r ⟨x, hx⟩,
+    apply subtype.coe_injective,
+    have := e.map_smul r ⟨x, mul_le_right R hx⟩,
+    apply_fun subtype.val at this,
+    exact this,
+  end }
+
+def linear_equiv.lmul (I : ideal R) {J K : ideal R} (e : J ≃ₗ[R] K) : 
+  (I * J : ideal R) ≃ₗ[R] (I * K : ideal R) :=
+{ inv_fun := linear_map.lmul I e.symm.to_linear_map,
+  left_inv := begin
+    intro x,
+    simp [linear_map.lmul],
+  end,
+  right_inv := begin
+    rintro ⟨x, hx⟩,
+    simp [linear_map.lmul],
+  end,
+  ..linear_map.lmul I e.to_linear_map }
+
+variable (R)
+
+-- It's true
+def con : con (ideal R) :=
 { mul' := begin
     rintros I J K L ⟨eIJ⟩ ⟨eKL⟩,
     refine ⟨_⟩,
-    -- Question (I don't know the answer) : Is this true in general?
-    -- It's true if R is a Dedekind domain; this is all implicit
-    -- in the algebraic number theory course 
-    /-
-    I J K L : ideal R
-    eIJ : ↥I ≃ₗ[R] ↥J
-    eKL : ↥K ≃ₗ[R] ↥L
-    ⊢ nonempty (↥(I * K) ≃ₗ[R] ↥(J * L))
-   -/
-    sorry
+    refine (ideal.linear_equiv.rmul K eIJ).trans (ideal.linear_equiv.lmul J eKL),
   end,
   ..ideal.s R }
 
 attribute [instance] ideal.s
 
-/- I can't do this; maybe it's not possible.
+abbreviation Picard_monoid := (ideal.con R).quotient
 
-## The maths question
+example : monoid ((ideal.con R).quotient) := con.monoid (ideal.con R)
 
-If I,J,K,L are ideals of R and I and J are abstractly isomorphic
-as R-modules, and K and L are also isomorphic. Is `IK` isomorphic
-to `JL`?
+instance : monoid (ideal.Picard_monoid R) := con.monoid (ideal.con R)
 
--/
+-- The ideal-theoretic definition of the Picard group
+abbreviation Picard_group := units (ideal.Picard_monoid R)
 
--- I can't fill this in because I can't answer the above question.
---def mul (A B : ideal.Picard_monoid R) : ideal.Picard_monoid R :=
-
--- However perhaps we can still define the Picard group.
--- Let's stick to invertible ideals.
+instance : group (ideal.Picard_group R) := infer_instance -- works
 
 -- notation
 open_locale tensor_product
